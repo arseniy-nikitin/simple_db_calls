f6bf6a10daffc5e508a9f6996d2fe5d13cfa5961
arseniy-nikitin <arseni04n@yandex.ru>
2024-03-26 02:28:26 +0300

    develop: These spaces was really important

--- src/workers_queries.hpp
+++ src/workers_queries.hpp
@@ -6,28 +6,28 @@
 namespace simple_db_calls {
   
   const userver::storages::postgres::Query kCreateWorkerQuery {
-      "INSERT INTO workers_schema.workers(position) VALUES($1)"
-      "ON CONFLICT (position)"
-      "DO NOTHING"
+      "INSERT INTO workers_schema.workers(position) VALUES($1) "
+      "ON CONFLICT (position) "
+      "DO NOTHING "
       "RETURNING workers.id",
       userver::storages::postgres::Query::Name{"create_worker_query"}};
 
   const userver::storages::postgres::Query kGetWorkerQuery {
-      "SELECT workers.position"
-      "FROM workers_schema.workers"
+      "SELECT workers.position "
+      "FROM workers_schema.workers "
       "WHERE workers.id = $1",
       userver::storages::postgres::Query::Name{"get_worker_query"}};
 
   const userver::storages::postgres::Query kUpdateWorkerQuery {
-      "UPDATE workers_schema.workers"
-      "SET workers.position = $1"
-      "WHERE workers.id = $2"
+      "UPDATE workers_schema.workers "
+      "SET workers.position = $1 "
+      "WHERE workers.id = $2 "
       "RETURNING workers.id",
       userver::storages::postgres::Query::Name{"update_worker_query"}};
 
   const userver::storages::postgres::Query kDeleteWorkerQuery {
-      "DELETE FROM workers_schema.workers"
-      "WHERE workers.id = $1"
+      "DELETE FROM workers_schema.workers "
+      "WHERE workers.id = $1 "
       "RETURNING workers.position",
       userver::storages::postgres::Query::Name{"delete_worker_query"}};
 
