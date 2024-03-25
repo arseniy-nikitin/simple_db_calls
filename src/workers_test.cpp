63179bfcd277036b234744e679a3ff89e5fa4525
arseniy-nikitin <arseni04n@yandex.ru>
2024-03-25 10:35:31 +0300

    develop: What is going on

--- src/workers_test.cpp
+++ src/workers_test.cpp
@@ -2,44 +2,67 @@
 
 #include <userver/utest/utest.hpp>
 
-UTEST(CreateWorker, Basic) {
-  using simple_db_calls::CreateWorker;
+namespace {
 
+class WorkersTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    workers_ = std::make_unique<simple_db_calls::Workers>(
+        userver::components::ComponentConfig{},
+        userver::components::ComponentContext{});
+  }
+
+  simple_db_calls::Workers* workers_ = nullptr;
+};
+
+}  // namespace
+
+UTEST_F(WorkersTest, CreateWorker_Basic) {
   workers::api::Worker worker;
   worker.set_id(1);
   worker.set_position("Senior Latte Drinker");
 
-  int32_t result = CreateWorker(worker);
+  workers::api::CreateWorkerRequest request;
+  request.set_allocated_worker(&worker);
 
-  EXPECT_EQ(result, 1);
+  workers::api::CreateWorkerResponse response;
+  workers_->CreateWorker(nullptr, std::move(request), &response);
+
+  EXPECT_EQ(response.id(), 1);
 }
 
-UTEST(GetWorker, Basic) {
-  using simple_db_calls::GetWorker;
+UTEST_F(WorkersTest, GetWorker_Basic) {
+  workers::api::GetWorkerRequest request;
+  request.set_id(1);
 
-  workers::api::Worker result = GetWorker(1);
+  workers::api::GetWorkerResponse response;
+  workers_->GetWorker(nullptr, std::move(request), &response);
 
-  EXPECT_EQ(result.id(), 1);
-  EXPECT_EQ(result.position(), "Senior Latte Drinker");
+  EXPECT_EQ(response.worker().id(), 1);
+  EXPECT_EQ(response.worker().position(), "Senior Latte Drinker");
 }
 
-UTEST(UpdateWorker, Basic) {
-  using simple_db_calls::UpdateWorker;
-
+UTEST_F(WorkersTest, UpdateWorker_Basic) {
   workers::api::Worker worker;
   worker.set_id(1);
   worker.set_position("Junior Latte Drinker");
 
-  int32_t result = UpdateWorker(worker);
+  workers::api::UpdateWorkerRequest request;
+  request.set_allocated_worker(&worker);
 
-  EXPECT_EQ(result, 1);
+  workers::api::UpdateWorkerResponse response;
+  workers_->UpdateWorker(nullptr, std::move(request), &response);
+
+  EXPECT_EQ(response.id(), 1);
 }
 
-UTEST(DeleteWorker, Basic) {
-  using simple_db_calls::DeleteWorker;
+UTEST_F(WorkersTest, DeleteWorker_Basic) {
+  workers::api::DeleteWorkerRequest request;
+  request.set_id(1);
 
-  workers::api::Worker result = DeleteWorker(1);
+  workers::api::DeleteWorkerResponse response;
+  workers_->DeleteWorker(nullptr, std::move(request), &response);
 
-  EXPECT_EQ(result.id(), 1);
-  EXPECT_EQ(result.position(), "Junior Latte Drinker");
+  EXPECT_EQ(response.worker().id(), 1);
+  EXPECT_EQ(response.worker().position(), "Junior Latte Drinker");
 }
