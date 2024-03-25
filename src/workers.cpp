e22251ed4115d48dfb80f835aa8b4a64e8b58d38
arseniy-nikitin <arseni04n@yandex.ru>
2024-03-25 06:56:18 +0300

    develop: Return queries back

--- src/workers.cpp
+++ src/workers.cpp
@@ -6,36 +6,6 @@
 #include <userver/storages/postgres/cluster.hpp>
 #include <userver/utils/assert.hpp>
 
-namespace {
-  
-  const storages::postgres::Query kCreateWorkerQuery {
-      "INSERT INTO workers_schema.workers(position) VALUES($1)"
-      "ON CONFLICT (position)"
-      "DO NOTHING"
-      "RETURNING workers.id",
-      storages::postgres::Query::Name{"create_worker_query"}};
-
-  const storages::postgres::Query kGetWorkerQuery {
-      "SELECT *"
-      "FROM workers_schema.workers"
-      "WHERE workers.id = $1",
-      storages::postgres::Query::Name{"get_worker_query"}};
-
-  const storages::postgres::Query kUpdateWorkerQuery {
-      "UPDATE workers_schema.workers"
-      "SET workers.position = $1"
-      "WHERE workers.id = $2"
-      "RETURNING workers.id",
-      storages::postgres::Query::Name{"update_worker_query"}};
-
-  const storages::postgres::Query kDeleteWorkerQuery {
-      "DELETE FROM workers_schema.workers"
-      "WHERE workers.id = $1"
-      "RETURNING *",
-      storages::postgres::Query::Name{"delete_worker_query"}};
-
-}  // namespace
-
 namespace simple_db_calls {
 
 void Workers::CreateWorker(workers::api::WorkersServiceBase::CreateWorkerCall& call,
---
+++ src/workers_queries.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <userver/storages/postgres/component.hpp>
+#include <userver/storages/postgres/query.hpp>
+
+namespace simple_db_calls {
+  
+  const userver::storages::postgres::Query kCreateWorkerQuery {
+      "INSERT INTO workers_schema.workers(position) VALUES($1)"
+      "ON CONFLICT (position)"
+      "DO NOTHING"
+      "RETURNING workers.id",
+      userver::storages::postgres::Query::Name{"create_worker_query"}};
+
+  const userver::storages::postgres::Query kGetWorkerQuery {
+      "SELECT *"
+      "FROM workers_schema.workers"
+      "WHERE workers.id = $1",
+      userver::storages::postgres::Query::Name{"get_worker_query"}};
+
+  const userver::storages::postgres::Query kUpdateWorkerQuery {
+      "UPDATE workers_schema.workers"
+      "SET workers.position = $1"
+      "WHERE workers.id = $2"
+      "RETURNING workers.id",
+      userver::storages::postgres::Query::Name{"update_worker_query"}};
+
+  const userver::storages::postgres::Query kDeleteWorkerQuery {
+      "DELETE FROM workers_schema.workers"
+      "WHERE workers.id = $1"
+      "RETURNING *",
+      userver::storages::postgres::Query::Name{"delete_worker_query"}};
+
+}  // namespace simple_db_calls
